a8138399c7682ceaa5704e94ff77559ff485d3f2
Christopher Felegy <chris@felegy.net>
2022-11-28 17:12:10 -0500

    replace GIT_COMMIT_HASH_SHORT with 7-length hash

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import simpleGit from "simple-git";
 
 export default new Transformer({
   async transform({ asset }) {
-    simpleGit().log({
-      maxCount: 1
-    });
+    const git = simpleGit();
+    const log = await git.log({maxCount: 1});
+    const hash = log.latest.hash.slice(0, 7);
+
     const source = await asset.getCode();
     
     asset.setCode(
-      source.replace(/GIT_COMMIT_HASH_SHORT/g, '000')
+      source.replace(/GIT_COMMIT_HASH_SHORT/g, hash)
     );
 
     return [asset];
